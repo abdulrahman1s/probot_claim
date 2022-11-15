65887fdc04836f7d9bf255308aafd0b0ec32dd54
Mr.Kasper <63007978+z7pz@users.noreply.github.com>
2022-11-15 18:38:12 +0300

    fix: handle all errors and skip them (#6)

--- src/claim.rs
+++ src/claim.rs
@@ -5,7 +5,7 @@ use serde_json::Value;
 
 use crate::utils::screenshot;
 
-pub fn spawn_calim(token: String, path: &str) {
+pub fn spawn_calim(token: String, path: &str) -> Result<(), String> {
     let probot_daily = String::from("https://probot.io/daily");
     let browser = Browser::new(
         LaunchOptions::default_builder()
@@ -19,26 +19,29 @@ pub fn spawn_calim(token: String, path: &str) {
     .unwrap();
     let tab = browser.wait_for_initial_tab().unwrap();
     tab.navigate_to(&probot_daily)
-        .unwrap_or_else(|_| panic!("couldn't navigate to \"{}\"!", &probot_daily));
+        .map_err(|_| format!("couldn't navigate to \"{}\"!", &probot_daily))?;
     tab.wait_for_element("body")
-        .unwrap()
+        .map_err(|_| format!("couldn't find the body..."))?
         .call_js_fn(
             r#"function locals (token) {
     localStorage.setItem("ac", token)
 }"#,
-            vec![Value::String(token.clone().to_string())],
+            vec![Value::String(token.to_string())],
             false,
         )
-        .unwrap();
+        .map_err(|_| format!("couldn't set the localstorage (ac = token)..."))?;
+
     tab.navigate_to(&probot_daily)
-        .unwrap_or_else(|_| panic!("couldn't navigate to \"{}\"!", &probot_daily));
-    tab.wait_for_element(".sidebar_ltr__kXJvp ").unwrap();
+        .map_err(|_| format!("couldn't navigate to \"{}\"!", &probot_daily))?;
+    tab.wait_for_element(".sidebar_ltr__kXJvp ")
+        .map_err(|_| format!("couldn't find sidebar (means u logged in)..."))?;
     tab.wait_for_element(".daily-logo-text")
-        .unwrap()
+        .map_err(|_| format!("couldn't find daily logo..."))?
         .click()
-        .unwrap();
+        .map_err(|_| format!("couldn't click daily logo..."))?;
     check(&tab);
     screenshot(&tab, token);
+    Ok(())
 }
 
 fn check(tab: &Tab) -> &Tab {
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 pub mod claim;
 pub mod utils;
 use claim::spawn_calim;
-use utils::{get_tokens, get_chrome_extionsion_path};
+use utils::{get_chrome_extionsion_path, get_tokens};
 #[tokio::main]
 async fn main() {
     let chrome_extionsion_path = get_chrome_extionsion_path();
     let tokens = get_tokens();
     for token in tokens {
-        spawn_calim(token.to_string(), chrome_extionsion_path.as_str());
+        spawn_calim(token.to_string(), chrome_extionsion_path.as_str())
+            .unwrap_or_else(|err| println!("{err}"))
     }
 }
